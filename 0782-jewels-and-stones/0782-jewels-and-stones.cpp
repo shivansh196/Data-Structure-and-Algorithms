0e8c1702bbc0c6d0648ad26cf95580f31c2272ad
shivansh196 <93548281+shivansh196@users.noreply.github.com>
2024-08-12 01:42:13 +0530

    Time: 0 ms (100%), Space: 8.9 MB (6.23%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_map<char,int> mp;
+        for(int i=0;i<stones.size();i++){
+            mp[stones[i]]++;
+        }
+        int sum =0;
+        for(int i=0;i<jewels.size();i++){
+         sum = sum+   mp[jewels[i]];
+        }
+        return sum;
+    }
+};
