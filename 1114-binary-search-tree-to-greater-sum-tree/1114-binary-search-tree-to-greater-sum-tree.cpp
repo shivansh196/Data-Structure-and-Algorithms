1c169f8069b590016f559d2b43d2ed1369db4fed
Shivansh Bhardwaj <93548281+shivansh196@users.noreply.github.com>
2024-08-16 13:24:30 +0530

    Time: 0 ms (100%), Space: 10.6 MB (5.58%) - LeetHub

--- 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.cpp
@@ -12,37 +12,22 @@
 class Solution {
 public:
 int sum=0;
-    void recurr(TreeNode* root,vector<int> &preo,int i){
-        if(root==NULL || i==(preo.size()-1)){
-            return ;
-        }
-     
-        
-        recurr(root->left,preo,i+1);
-           root->val = preo[i];
-        recurr(root->right,preo,i+1);
-    }
-    TreeNode* preorder(TreeNode* root,vector<int> &preo){
+   
+    TreeNode* preorder(TreeNode* root){
         if(root==NULL){
             return root ;
         }
      
-      preorder(root->right,preo);
+      preorder(root->right);
       sum = sum + root->val;
       root->val = sum;
-    //   preo.push_back(root->val);
-      preorder(root->left,preo);
+   
+      preorder(root->left);
       return root;
     }
     TreeNode* bstToGst(TreeNode* root) {
-        vector<int> preo;
-         return preorder(root,preo);
-        for(int i=preo.size()-1;i>=0;i--){
-            int sum = sum+preo[i];
-            preo[i]=sum;
-        }
-      int i=0;
-      //  recurr(root,preo,i);
-       // return root;
+       
+         return preorder(root);
+       
     }
 };
