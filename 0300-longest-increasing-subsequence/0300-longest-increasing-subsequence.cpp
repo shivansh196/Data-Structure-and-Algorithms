b72502efde5ab22efe705fbbde072e2d267ad503
Shivansh Bhardwaj <93548281+shivansh196@users.noreply.github.com>
2024-09-06 22:31:57 +0530

    Time: 202 ms (40.95%), Space: 294.9 MB (29%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -21,18 +21,33 @@ public:
     //     int n= nums.size();
     //     vector<vector<int>> dp(n,vector<int>(n+1,-1));
     //    return recurr(nums,prev,i,dp);
-    vector<int> ans;
-    ans.push_back(nums[0]);
-    for(int i=1;i<nums.size();i++){
-         if(nums[i]>ans.back()){
-            ans.push_back(nums[i]);
-         }
-         else{
-             int temp = lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
-           // int temp = binary
-            ans[temp]=nums[i];
-         }
+    // vector<int> ans;
+    // ans.push_back(nums[0]);
+    // for(int i=1;i<nums.size();i++){
+    //      if(nums[i]>ans.back()){
+    //         ans.push_back(nums[i]);
+    //      }
+    //      else{
+    //          int temp = lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+    //        // int temp = binary
+    //         ans[temp]=nums[i];
+    //      }
+    // }
+    // return ans.size();
+    int ans =0;
+    int n = nums.size();
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    for(int i=nums.size()-1;i>=0;i--){
+        for(int j=i-1;j>=-1;j--){
+            int nottake = dp[i+1][j+1];
+
+            int take =0;
+            if(j==-1 || nums[i]>nums[j])
+            take = 1+ dp[i+1][i+1];
+
+            dp[i][j+1]= max(take,nottake);
+        }
     }
-    return ans.size();
+    return dp[0][0];
     }
 };
