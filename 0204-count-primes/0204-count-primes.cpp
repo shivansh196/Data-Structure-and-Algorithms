fd334ff80e69fc2a3c926eaf5f0e72ff2ed624e5
Shivansh Bhardwaj <93548281+shivansh196@users.noreply.github.com>
2024-09-09 23:22:57 +0530

    Time: 420 ms (58.82%), Space: 11.8 MB (79.34%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+    if(n<=2)return 0;
+    vector<bool>v(n,true)    ;
+    v[0]=false;
+    v[1]=false;
+    for(int i=2;i*i<n;i++)
+    {
+        if(v[i])
+        {
+            // make multiple of that into false;
+            for(int j=i*i;j<n;j+=i)
+            {
+                v[j]=false;
+            }
+        }
+    }
+    int count=0;
+    for(int i=2;i<n;i++)
+    {
+        if(v[i])count++;
+    }
+        return count;
+    
+
+    }
+};
