8f9df3a1794d8a2344bf6cef722a32e7d20f1201
Shivansh Bhardwaj <93548281+shivansh196@users.noreply.github.com>
2024-10-03 22:22:12 +0530

    Time: 21 ms (29.62%), Space: 28 MB (32.92%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -5,13 +5,16 @@ public:
         unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
-        }
-        for(auto it:mp){
-            if(it.second>(n/2)){
-                return it.first;
-                
+            if(mp[nums[i]]>(n/2)){
+                return nums[i];
             }
         }
+        // for(auto it:mp){
+        //     if(it.second>(n/2)){
+        //         return it.first;
+                
+        //     }
+       // }
         return 0;
     }
 };
