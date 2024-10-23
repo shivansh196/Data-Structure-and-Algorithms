772a46eab5ded6b05af58ea9ac56cdcefec3e701
Shivansh Bhardwaj <93548281+shivansh196@users.noreply.github.com>
2024-10-23 12:04:14 +0530

    Time: 2 ms (72.81%), Space: 9.3 MB (44.84%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans;
+        vector<int> ans(n+1);
         for(int i=0;i<=n;i++){
-            ans.push_back(__builtin_popcount(i));
+              ans[i]=__builtin_popcount(i);
         }
         return ans;
     }
