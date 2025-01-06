8adfb3938ef44a27bd10c186170c8b25d6c944f6
Shivansh Bhardwaj <93548281+shivansh196@users.noreply.github.com>
2025-01-06 22:49:13 +0530

    Time: 0 ms (100%), Space: 19 MB (50.06%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,24 +11,15 @@
  */
 class Solution {
 public:
-    int recurr(TreeNode* root){
+      int recurr(TreeNode* root){
         if(root==NULL){
             return 0;
         }
-        int left=0;
-        int right =0;
-        if(root->left!=NULL){
-            left = 1+ recurr(root->left); 
-        }
-        if(root->right!=NULL){
-        right = 1+ recurr(root->right);
-        }
+        int left = 1+recurr(root->left);
+        int right = 1+ recurr(root->right);
         return max(left,right);
-    }
+      }
     int maxDepth(TreeNode* root) {
-        if(root==NULL){
-            return 0;
-        }
-       return  recurr(root)+1;
+        return recurr(root);
     }
 };
